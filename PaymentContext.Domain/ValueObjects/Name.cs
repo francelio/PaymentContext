060dafc8ca0dacf0ab52dc801b837c136b438e9f
Francelio Alencar <francelio.si@gmaiil.com>
2019-07-08 07:46:32 -0300

    Fail Fast Validations

--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -4,17 +4,17 @@ using PaymentContext.Shared.ValueObjects;
 namespace PaymentContext.Domain.ValueObjects {
     public class Name : ValueObject {
         public Name (string fistName, string lastName) {
-            FistName = fistName;
+            FirstName = fistName;
             LastName = lastName;    
             AddNotifications (new Contract ()
                 .Requires ()
-                .HasMinLen (FistName,3,"Name.FistName", "Nome deve conter pelo menos 3 caracteres ")
+                .HasMinLen (FirstName,3,"Name.FistName", "Nome deve conter pelo menos 3 caracteres ")
                 .HasMinLen (LastName,3,"Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres ")
-                .HasMaxLen (FistName,40,"Name.FistName", "Nome deve conter até 40 caracteres "));
+                .HasMaxLen (FirstName,40,"Name.FistName", "Nome deve conter até 40 caracteres "));
   
         }
 
-        public string FistName { get; private set; }
+        public string FirstName { get; private set; }
         public string LastName { get; private set; }
     }
 }
